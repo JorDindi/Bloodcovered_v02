db1dacd518448e1b44672219ba7403d5b222012f
Uriah Michaeli <uriahmic@gmail.com>
2020-04-22 23:59:58 +0300

    add enemy partol and attack states

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -7,25 +7,23 @@ using UnityEngineInternal;
 public class EnemyController : MonoBehaviour
 {
     public Sprite deathSprite;
-    public Transform firePoint;
-    public GameObject projectilePrefab;
-    public Transform target;
-
-    public float projectileForce = 20f;
+    //public Transform firePoint;
+    //public GameObject projectilePrefab;
+    //public Animator animator;
+    private Animator animator;
+    //public float projectileForce = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
         Physics2D.queriesStartInColliders = false;
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Shoot();
-        }
+
     }
 
     void FixedUpdate()
@@ -38,6 +36,8 @@ public class EnemyController : MonoBehaviour
         {
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
             Debug.Log("Player detected, distance is " + distance);
+            animator.SetTrigger("Attack");
+            //gameObject.GetComponent<EnemyShoot>().enabled = true;
         }
     }
 
@@ -49,13 +49,5 @@ public class EnemyController : MonoBehaviour
             GetComponent<SpriteRenderer>().sortingOrder = -2;
             Destroy(GetComponent<CapsuleCollider2D>());
         }
-    }
-
-    void Shoot()
-    {
-        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-        //GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * projectileForce, ForceMode2D.Impulse);
     }
 }
