b0ef7ce4238680135e39e18632cc716938916597
Alon <alonbra@post.bgu.ac.il>
2020-04-30 17:39:27 +0300

    Added "Game Over" condition + updated sprites

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartLevel : MonoBehaviour
+{
+   [SerializeField] private Text restartText;
+   [SerializeField] private PlayerMovement playerMov;
+   [SerializeField] private SpriteRenderer playerSprite;
+   [SerializeField] private PlayerAttack _playerAttack;
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+      if (other.gameObject.CompareTag("Projectile"))
+      {
+         restartText.enabled = true;
+         playerMov.enabled = false;
+         playerSprite.enabled = false;
+         _playerAttack.enabled = false;
+         if (Input.GetKeyDown(KeyCode.R))
+         {
+            SceneManager.LoadScene("CurrentWorkingScene", LoadSceneMode.Single);
+         }
+      }
+   }
+}
